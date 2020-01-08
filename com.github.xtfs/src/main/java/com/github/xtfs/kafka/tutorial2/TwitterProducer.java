5bd9cae18d83ec28d03e867561f45738914caf46
Tiago Farias <sarcer@gmail.com>
2020-01-07 23:35:19 -0300

    Add twitterProducer

---
+++ com.github.xtfs/src/main/java/com/github/xtfs/kafka/tutorial2/TwitterProducer.java
@@ -0,0 +1,79 @@
+package com.github.xtfs.kafka.tutorial2;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+    private Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+    private String consumerKey = "MGqQWB5nvOPUrQn55jp7uAgjy";
+    private String consumerSecret = "eD0XyUWFXmO2qZxZ741fIi7UoiW0bYNLuePFIVghr3iM2rQGDx";
+    private String token = "227090498-7Ujk5CX7eRy1s5rG1ThZzigeY3BxXemeRtJ8GHnh";
+    private String secret = "CqupIjO3CarZksC7DNQU5xJKonNfCu73EcCpK3MDAjDox";
+
+    public TwitterProducer(){}
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    private void run() {
+        logger.info("Setup");
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(10);
+        Client client = this.createTwitterClient(msgQueue);
+        client.connect();
+
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(50, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                logger.error("Error on poll twitters", e);
+            }
+
+            if (msg != null) {
+                logger.info(msg);
+            }
+        }
+
+        logger.info("Read finished");
+    }
+
+    private Client createTwitterClient(BlockingQueue<String> msgQueue) {
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        // Optional: set up some followings and track terms
+        List<String> terms = Lists.newArrayList("amazon");
+        hosebirdEndpoint.trackTerms(terms);
+
+        Authentication hosebirdAuth = new OAuth1(this.consumerKey, this.consumerSecret, this.token, this.secret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        return builder.build();
+    }
+
+}
